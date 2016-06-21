eaad0904ec22ea6db98796725488ff347b2963b7
marchurley <marc@swiss-magic.net>
2016-06-21 17:57:51 +0200

    FizzBuzz

--- js/app.js
+++ js/app.js
@@ -1,98 +1,21 @@
 $(document).ready(function(){
 
-	//Define enterKey function on textInput
-	$("input[name=textInput]").keyup(function(e){
-		if(e.keyCode==13)
-		{
-			$(this).trigger("enterKey");
+	var i = 1;
+
+	for (var i; i < 101; i++) {
+		if ((i % 5 === 0) && (i % 3 === 0)) {
+			$("ul").append("<li>" + "FizzBuzz" + "</li>");
 		}
-	});
-	//Add item when Enter is pressed on textInput
-	$("input[name=textInput]").bind("enterKey",function(e){
-		var listItem = $("input[name=textInput]").val();
-		$(".textOutput").append(
-			"<li>" + 
-			"<span class='item'>" +
-			listItem + 
-			"</span>" +
-			"<button class='check'>" + 
-			"&#10003" + 
-			"</button>" +
-			"<button class='delete'>" + 
-			"x" + 
-			"</button>" +
-			"</li>"
-			);
-	$("input[name=textInput]").val('');
-	});
-
-	
-	//Add item when +-button is clicked	
-	$("input[name=submit]").click(function(){
-		var listItem = $("input[name=textInput]").val();
-		$(".textOutput").append(
-			"<li>" + 
-			"<span class='item'>" +
-			listItem + 
-			"</span>" +
-			"<button class='check'>" + 
-			"&#10003" + 
-			"</button>" +
-			"<button class='delete'>" + 
-			"x" + 
-			"</button>" +
-			"</li>"
-			);
-		$("input[name=textInput]").val('');
-	});
-
-	// Add item to my bag
-	$("ul").on('click', '.check', (function(){
-		item = $(this).parent();
-		itemName = item.children(".item").html();
-		$(".inmybag").append(
-			"<li>" + 
-			"<span class='item'>" +
-			itemName + 
-			"</span>" +
-			"<button class='backup'>" + 
-			"⇧" + 
-			"</button>" +
-			"<button class='delete'>" + 
-			"x" + 
-			"</button>" +
-			"</li>"
-			);
-		item.remove();
-	}));
-
-
-	// Move item back to buy
-	$("ul").on('click', '.backup', (function(){
-		bagItem = $(this).parent();
-		bagItemName = bagItem.children(".item").html();
-		$(".textOutput").append(
-			"<li>" + 
-			"<span class='item'>" +
-			bagItemName + 
-			"</span>" +
-			"<button class='check'>" + 
-			"&#10003" + 
-			"</button>" +
-			"<button class='delete'>" + 
-			"x" + 
-			"</button>" +
-			"</li>"
-			);
-		bagItem.remove();
-	}));
-
-	//Remove item when x-button is clicked
-	$('ul').on('click', '.delete', (function(e){ 
-		$(this).parent().remove()
-	}));
-
-
+		else if ((i % 3) ===0) {
+			$("ul").append("<li>" + "Fizz" + "</li>");
+		}
+		else if ((i % 5) === 0  ) {
+			$("ul").append("<li>" + "Buzz" + "</li>");
+		}
+		else {
+			$("ul").append("<li>" + i + "</li>");
+		}
+	}
 
 });
 
